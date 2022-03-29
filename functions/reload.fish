cd69c464034f6085ace89e254d580559bf79ef9d
e971 <e97@lllnoMacBook-Air.local>
2022-03-30 03:30:01 +0900

    fix: reload.fish

--- functions/reload.fish
+++ functions/reload.fish
@@ -1,19 +1,24 @@
 function reload
-    argparse -n bd -x 'c,s,i,h' \
+    argparse -n bd -x 'c,h' \
         c/config \
         h/help -- $argv
-    or return 1
+        or return 1
 
-    if set -lq _flag_config
-        source (string join '' $__fish_config_dir config.fish)
-    else if set -lq _fish_help
-        set -l help_msg "Description: just reload fish config files.
+    set -l help_msg "Description: just reload fish config files.
 
 Usage:
 reload [option]
 
 Options:
-    -c, --config        reload $__fish_config_dir/config.fish
+-c, --config        reload $__fish_config_dir/config.fish
 "
+
+    if set -lq _flag_help
+        echo $help_msg
+    else if set -lq _flag_config
+        source (string join '/' $__fish_config_dir config.fish)
+        echo reloaded $__fish_config_dir/config.fish
+    else
+        exec fish
     end
 end
