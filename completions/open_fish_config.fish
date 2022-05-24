d5c12df2a3ccac4b085632a03ee8769c9191f476
e9 <e4zy971@gmail.com>
2022-05-24 20:14:37 +0900

    fix: completions of open_fish_config and reload command

--- completions/open_fish_config.fish
+++ completions/open_fish_config.fish
@@ -1,5 +1,5 @@
-complete -c reload -s h -l help -x --description "display help message"
-complete -c reload -s c -l cat -x --description "cat or bat fish.config"
-complete -c reload -s c -l path -x --description "echo fish.config path"
+complete -c open_fish_config -s h -l help -x --description "display help message"
+complete -c open_fish_config -s c -l cat -x --description "cat or bat fish.config"
+complete -c open_fish_config -s c -l path -x --description "echo fish.config path"
 
 
