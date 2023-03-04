d4a7ca4383ff7148dfbadd3e6a37f43ddcf223f3
rai <mj82xx@gmail.com>
2023-03-04 18:30:05 +0900

    add completion to `do` command

---
+++ completions/do.fish
@@ -0,0 +1,15 @@
+function __available_scripts
+    set -l cwd (pwd)
+    set -l root (git rev-parse --show-toplevel 2>/dev/null)
+
+    set -l scripts
+    for script in (find $root/scripts -type f -name "*.sh")
+        set script (basename -s ".sh" "$script")
+        echo $script
+        set scripts $scripts $script
+    end
+    printf $scripts
+end
+
+complete -c do -f
+complete -c do -n "not __fish_seen_subcommand_from (__available_scripts)" -a "(__available_scripts)"
